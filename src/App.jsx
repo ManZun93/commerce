93b7b778a58c1ecdd980af677b132def842489a8
ManZun93 <manuellufan19@gmail.com>
2022-11-28 00:17:05 -0500

    esqueleto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProductDetail from './pages/ProductDetail'
+import Purchases from './pages/Purchases'
+import NavBar from './components/NavBar'
+import LoadingScreen from './components/LoadingScreen'
+import { useSelector } from 'react-redux'
+import { Container } from 'react-bootstrap'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const isLoading = useSelector((state) => state.isLoading);
 
   return (
-    <div className="App">
-      
-    </div>
+    <HashRouter>
+
+      <NavBar />
+      {isLoading && <LoadingScreen />}
+      <Container className='my-5'>
+        <Routes >
+          <Route path='/' element={<Home />} />
+          <Route path='/productdetail/:id' element={<ProductDetail />} />
+          <Route path='/purchases' element={<Purchases />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Container>
+
+    </HashRouter>
   )
 }
 
